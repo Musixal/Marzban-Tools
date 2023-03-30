6f29e98aae7ef0fcae6202bd1df59bb5c3fdd1fb
MusiX <124933548+MusiXal@users.noreply.github.com>
2023-03-31 01:47:38 +0330

    Update block_iranian_sites.sh

--- block-iranian-site/block_iranian_sites.sh
+++ block-iranian-site/block_iranian_sites.sh
@@ -14,7 +14,7 @@ wget -q -O /var/lib/marzban/assets/geosite.dat https://github.com/v2fly/domain-l
 wget -q -O /var/lib/marzban/assets/geoip.dat https://github.com/v2fly/geoip/releases/latest/download/geoip.dat
 wget -q -O /var/lib/marzban/assets/iran.dat https://github.com/bootmortis/iran-hosted-domains/releases/latest/download/iran.dat
 echo 'XRAY_ASSETS_PATH = "/var/lib/marzban/assets/"' >> env
-read -p 'Enter your website without https:// ' website
+read -p 'Enter your website without https:// : ' website
 rm xray_config.json
 wget -O xray_config.json https://raw.githubusercontent.com/MusiXal/Marzban-Tools/main/block-iranian-site/xray_config.json
 sed -i 's/www.example.com/'"$website"'/g' xrayconfig.json
