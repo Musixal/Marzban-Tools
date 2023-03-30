b52222129faea0cd4d3d0f7f7e2aaae44b3d06f3
MusiX <124933548+MusiXal@users.noreply.github.com>
2023-03-31 01:32:11 +0330

    Update block.sh

--- block-iranian-site/block.sh
+++ block-iranian-site/block.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 echo "Welcome to block-iranian-sites tools"
-echo "downloading necessary files"
+
+if [ ! -e env ]
+then
+    echo "WRONG DIRECTORY !"
+    exit
+fi
+
+echo "downloading necessary files..."
 mkdir -p /var/lib/marzban/assets/
 wget -O /var/lib/marzban/assets/geosite.dat https://github.com/v2fly/domain-list-community/releases/latest/download/dlc.dat
 wget -O /var/lib/marzban/assets/geoip.dat https://github.com/v2fly/geoip/releases/latest/download/geoip.dat
@@ -9,7 +16,9 @@ wget -O /var/lib/marzban/assets/iran.dat https://github.com/bootmortis/iran-host
 echo 'XRAY_ASSETS_PATH = "/var/lib/marzban/assets/"' >> env
 read -p 'Enter your website without https:// ' website
 rm xray_config.json
-
-
+wget -O xray_config.json https://raw.githubusercontent.com/MusiXal/Marzban-Tools/main/block-iranian-site/xray_config.json
+sed -i 's/www.example.com/'"$website"'/g' xrayconfig.json
+echo "Restaring Marzban..."
 docker compose down
 docker compose up -d
+echo "Done!"
