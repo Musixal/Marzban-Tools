bd1b12dbc94e3f6771d9293bbc28059eeca5c999
MusiX <124933548+MusiXal@users.noreply.github.com>
2023-03-31 02:06:24 +0330

    Update block_iranian_sites.sh

--- block-iranian-site/block_iranian_sites.sh
+++ block-iranian-site/block_iranian_sites.sh
@@ -15,10 +15,11 @@ curl -s https://github.com/v2fly/geoip/releases/latest/download/geoip.dat -O /va
 curl -s https://github.com/bootmortis/iran-hosted-domains/releases/latest/download/iran.dat -O /var/lib/marzban/assets/iran.dat
 echo 'XRAY_ASSETS_PATH = "/var/lib/marzban/assets/"' >> env
 read -p 'Enter your website without https:// : ' website
+echo -e "\n"
 rm xray_config.json
 curl -s https://raw.githubusercontent.com/MusiXal/Marzban-Tools/main/block-iranian-site/xray_config.json -O xray_config.json
 sed -i 's/www.example.com/'"$website"'/g' xray_config.json
-echo "Restaring Marzban..."
+echo -e "Restaring Marzban...\n"
 docker compose down
 docker compose up -d
 echo "Done!"
