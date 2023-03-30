e4f460fb8699a8f593a2e0a3137e613e9bd29f92
MusiX <124933548+MusiXal@users.noreply.github.com>
2023-03-31 02:03:09 +0330

    Update block_iranian_sites.sh

--- block-iranian-site/block_iranian_sites.sh
+++ block-iranian-site/block_iranian_sites.sh
@@ -10,13 +10,13 @@ fi
 
 echo -e "downloading necessary files... \n"
 mkdir -p /var/lib/marzban/assets/
-curl -s -O /var/lib/marzban/assets/geosite.dat https://github.com/v2fly/domain-list-community/releases/latest/download/dlc.dat
-curl -s -O /var/lib/marzban/assets/geoip.dat https://github.com/v2fly/geoip/releases/latest/download/geoip.dat
-curl -s -O /var/lib/marzban/assets/iran.dat https://github.com/bootmortis/iran-hosted-domains/releases/latest/download/iran.dat
+curl -s https://github.com/v2fly/domain-list-community/releases/latest/download/dlc.dat -O /var/lib/marzban/assets/geosite.dat 
+curl -s https://github.com/v2fly/geoip/releases/latest/download/geoip.dat -O /var/lib/marzban/assets/geoip.dat
+curl -s https://github.com/bootmortis/iran-hosted-domains/releases/latest/download/iran.dat -O /var/lib/marzban/assets/iran.dat
 echo 'XRAY_ASSETS_PATH = "/var/lib/marzban/assets/"' >> env
 read -p 'Enter your website without https:// : ' website
 rm xray_config.json
-curl -s -O xray_config.json https://raw.githubusercontent.com/MusiXal/Marzban-Tools/main/block-iranian-site/xray_config.json
+curl -s https://raw.githubusercontent.com/MusiXal/Marzban-Tools/main/block-iranian-site/xray_config.json -O xray_config.json
 sed -i 's/www.example.com/'"$website"'/g' xray_config.json
 echo "Restaring Marzban..."
 docker compose down
